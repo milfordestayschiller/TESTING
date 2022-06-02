b8e3e4ffe58bad79fecdcf6391faa4f5d52a5ee0
milfordestayschiller <103213100+milfordestayschiller@users.noreply.github.com>
2022-06-02 18:43:07 -0400

    Update
    Update

--- 03-Funciones1/funciones.js
+++ 03-Funciones1/funciones.js
@@ -1,14 +1,31 @@
-
-
 function crearCalculadora(calculadora) {
 let resultado = 0;
+
 let objetoARetornar = {
     valor: function (params) {
         return resultado
+        
+    },
+    
+    sumar: function (num){
+        resultado+=num
+    },
+
+    restar: function (num) {
+      resultado-=num
+    },
+
+    reset: function (params) {
+      return resultado = 0;
     }
+    
 }
 return objetoARetornar
 }
+
+
+
+
 
 
 
