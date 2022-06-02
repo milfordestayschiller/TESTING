e40252e06ffdaa898f6e73c7f96aaf671e1ee2fb
milfordestayschiller <103213100+milfordestayschiller@users.noreply.github.com>
2022-06-02 11:09:04 -0400

    Add files via upload

---
+++ 05-Constructores/constructores.js
@@ -0,0 +1,22 @@
+class CalculadoraNPI  {
+   constructor(calculadora) {
+       this.calculadora = calculadora;
+   }
+   agregar(num1) {
+       
+       return num1
+   }
+
+  sumar(valor) {
+   
+       return valor
+   }
+   
+valor (valor1,valor2) {
+return `${valor1+valor2}`
+}
+
+}
+
+var calculadora = new CalculadoraNPI("Calculadora que use notación polaca inversa")
+
