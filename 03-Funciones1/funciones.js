874a8f13dfdcd9e44e98f5c637f14d2b982d6a02
milfordestayschiller <103213100+milfordestayschiller@users.noreply.github.com>
2022-06-02 11:08:29 -0400

    Add files via upload

---
+++ 03-Funciones1/funciones.js
@@ -0,0 +1,15 @@
+
+
+function crearCalculadora(calculadora) {
+let resultado = 0;
+let objetoARetornar = {
+    valor: function (params) {
+        return resultado
+    }
+}
+return objetoARetornar
+}
+
+
+
+
