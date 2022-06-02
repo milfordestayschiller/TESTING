fe35254037f9d2b16c2473b415bfb7e7c046c5ab
milfordestayschiller <103213100+milfordestayschiller@users.noreply.github.com>
2022-06-02 11:07:15 -0400

    Add files via upload

---
+++ 00-ProbandoTests/test.js
@@ -0,0 +1,12 @@
+function sumar(a, b) {
+    return a+b;
+}
+
+function saludar(nombre) {
+    if(nombre) {
+        return ("Hola, "+ nombre + "!")
+    } else {
+        return "Hola!"
+    }
+    
+}
